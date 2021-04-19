d36dada8e9034530a688cf6c57fb3e4dd7fa8313
leilahelhajjamy <48299336+leilahelhajjamy@users.noreply.github.com>
2021-04-19 14:59:00 +0200

    Delete src/app/config directory

--- src/app/config/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDRXaXw08GIFBBNw6wrgU6dxnahPeb4oM4",
-  authDomain: "chauffageapp.firebaseapp.com",
-  projectId: "chauffageapp",
-  storageBucket: "chauffageapp.appspot.com",
-  messagingSenderId: "146798707197",
-  appId: "1:146798707197:web:f27c388c61eefc43bf1994",
-};
